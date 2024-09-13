0984d7e9b484a5933d6cf4530707bccf9805bc20
Nicou11 <lyoungin1204@gmail.com>
2024-09-13 17:16:54 +0900

    test3

--- src/samdul06food/main.py
+++ src/samdul06food/main.py
@@ -9,7 +9,6 @@ import csv
 app = FastAPI()
 
 origins = [
-        "http://localhost:8006",
         "http://127.0.0.1:8899",
         "https://samdul06food.web.app",
 ]
@@ -22,8 +21,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-home_path = os.path.expanduser("~")
-file_path = f"{home_path}/code/data/food/"
+#home_path = os.path.expanduser("~")
+file_path = "/code/data/food.csv"
 if not os.path.exists(file_path):
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
@@ -37,22 +36,14 @@ def food(name: str):
     real_time = time.strftime('%Y-%m-%d %H:%M:%S')
     data = {"food": name, "time": real_time}
 
-    file_name = f"{file_path}/{day}.csv"
+    #file_name = f"{file_path}/{day}.csv"
+    file_name = "food.csv"
     
     # CSV 파일이 존재하는지 확인
-    file_exists = os.path.isfile(file_name)
+    file_exists = os.path.isfile(file_path)
     
     # CSV 파일에 데이터 추가
-    with open(file_name, mode='a', newline='', encoding='utf-8') as f:
+    with open(file_path, mode='a', newline='', encoding='utf-8') as f:
         writer = csv.DictWriter(f, fieldnames=["food", "time"])
-        # 파일이 처음 작성되는 경우 헤더를 추가
-        if not file_exists:
-            writer.writeheader()
-        # 데이터 행 추가
         writer.writerow(data)
-    print("========================" + name )
     return {"food": name, "time": real_time}
-    # 시간을 구함
-    # 음식 이름과 시간을 csv 로 저장 -> /code/data/food.csv
-
-#food("chicken")
