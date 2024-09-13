477951c11a44774724525d54cfab4677e05d877d
Nicou11 <lyoungin1204@gmail.com>
2024-09-13 12:49:13 +0900

    add CORS

--- src/samdul06food/main.py
+++ src/samdul06food/main.py
@@ -1,9 +1,22 @@
 from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import pickle
 import time
 
 app = FastAPI()
+
+origins = [
+        "http://localhost:8899.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
@@ -13,4 +26,5 @@ def read_root():
 def food(name: str):
     # 시간을 구함
     # 음식 이름과 시간을 csv 로 저장 -> /code/data/food.csv
+    print("========================" + name )
     return {"food": name, "time": time.strftime('%Y-%m-%d %H:%M:%S')}
