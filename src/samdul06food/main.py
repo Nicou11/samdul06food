2d7ed31c25b67e4e08a97260fe22c7672d51fcf6
Nicou11 <lyoungin1204@gmail.com>
2024-09-13 14:47:30 +0900

    add mkdir

--- src/samdul06food/main.py
+++ src/samdul06food/main.py
@@ -3,12 +3,13 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import pickle
 import time
+import os
+import json
 
 app = FastAPI()
 
 origins = [
         "http://localhost:8006",
-        "http://localhost:8899",
         "http://127.0.0.1:8899",
         "https://samdul06food.web.app",
 ]
@@ -21,12 +22,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+home_path = os.path.expanduser("~")
+file_path = f"{home_path}/code/data/food"
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "n06"}
 
 @app.get("/food")
 def food(name: str):
+    if not os.path.exists(file_path):
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
     # 시간을 구함
     # 음식 이름과 시간을 csv 로 저장 -> /code/data/food.csv
     print("========================" + name )
