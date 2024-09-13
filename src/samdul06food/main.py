2e8580a5fd1e4820a353763e9e3f13c7636d05cd
Nicou11 <lyoungin1204@gmail.com>
2024-09-13 16:30:32 +0900

    save csv

--- src/samdul06food/main.py
+++ src/samdul06food/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import pickle
 import time
 import os
-import json
+import csv 
 
 app = FastAPI()
 
@@ -36,9 +36,23 @@ def food(name: str):
     day = time.strftime('%Y-%m-%d')
     real_time = time.strftime('%Y-%m-%d %H:%M:%S')
     data = {"food": name, "time": real_time}
-    with open(f"{file_path}/{day}", 'w') as f:
-        json.dump(data, f)
-    # 시간을 구함
-    # 음식 이름과 시간을 csv 로 저장 -> /code/data/food.csv
+
+    file_name = f"{file_path}/{day}.csv"
+    
+    # CSV 파일이 존재하는지 확인
+    file_exists = os.path.isfile(file_name)
+    
+    # CSV 파일에 데이터 추가
+    with open(file_name, mode='a', newline='', encoding='utf-8') as f:
+        writer = csv.DictWriter(f, fieldnames=["food", "time"])
+        # 파일이 처음 작성되는 경우 헤더를 추가
+        if not file_exists:
+            writer.writeheader()
+        # 데이터 행 추가
+        writer.writerow(data)
     print("========================" + name )
     return {"food": name, "time": real_time}
+    # 시간을 구함
+    # 음식 이름과 시간을 csv 로 저장 -> /code/data/food.csv
+
+food("chicken")
