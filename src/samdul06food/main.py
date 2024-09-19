361d9b02d3738540d432ca6c2dd332f9a4887a7b
Nicou11 <lyoungin1204@gmail.com>
2024-09-19 12:27:15 +0900

    aws 배포

--- src/samdul06food/main.py
+++ src/samdul06food/main.py
@@ -24,8 +24,8 @@ app.add_middleware(
 )
 
 home_path = os.path.expanduser("~")
-file_path = "{home_path}/code/data/food.csv"  # local test용
-#file_path = "/code/data/food.csv"    # AWS 배포용
+#file_path = "{home_path}/code/data/food.csv"  # local test용
+file_path = "/code/data/food.csv"    # AWS 배포용
 if not os.path.exists(file_path):
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
